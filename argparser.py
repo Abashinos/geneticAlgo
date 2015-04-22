8d098001f6828ae545389c13c0780a6ca22caae5
i.abashin <abashinos@gmail.com>
2015-04-22 17:14:40 +0300

    mutations

--- argparser.py
+++ argparser.py
@@ -5,8 +5,9 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description='N queens')
 
-    parser.add_argument('-N', action='store', type=int, dest='size', required=True, help='Board size')
+    parser.add_argument('-N', action='store', type=int, dest='board_size', required=True, help='Board size')
     parser.add_argument('-L', action='store', type=long, dest='limit', required=True, help='Population limit')
-    parser.add_argument('-R', action='store', type=int, dest='retries', required=True, help='Retries limit')
+    parser.add_argument('-M', action='store', type=int, dest='mutation', help='Mutation chance percent')
+    parser.add_argument('-R', action='store', type=int, dest='retries', help='Retries limit')
 
     return parser.parse_args()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from copy import copy
 import random
 import sys
+import time
 from argparser import parse_args
 
 
-def get_children(parents, limit):
+def get_children(parents, limit, board_size, mutation=0):
     total_errors = 0
     for parent in parents:
         parent['errors'] = 0
@@ -13,24 +14,24 @@ def get_children(parents, limit):
                 if (j - i) == abs(parent['places'][j] - parent['places'][i]):
                     parent['errors'] += 1
         total_errors += parent['errors']
-    total_errors //= N-1
+    total_errors //= board_size-1
 
     ok_parents = []
     for parent in parents:
         if parent['errors'] <= total_errors:
             ok_parents.append(parent)
-    print(len(ok_parents))
+
+    random.shuffle(ok_parents)
 
     children = []
+    min_error = ([], sys.maxint)
     for i in range(0, len(ok_parents)-1):
         for j in range(i+1, len(ok_parents)):
-            if len(children) >= limit:
-                break
 
             child = {'id': i, 'parents': (i, j), 'errors': 0, 'places': copy(ok_parents[i]['places'])}
             fixed = []
 
-            for k in range(0, N):
+            for k in range(0, board_size):
                 if ok_parents[i]['places'][k] == ok_parents[j]['places'][k]:
                     fixed.append((k, ok_parents[i]['places'][k]))
 
@@ -40,41 +41,54 @@ def get_children(parents, limit):
                 index = child['places'].index(item)
                 child['places'][pos], child['places'][index] = child['places'][index], child['places'][pos]
 
+            if mutation >= random.randint(1, 100):
+                a = random.randint(0, board_size-1)
+                b = random.randint(0, board_size-1)
+                child['places'][a], child['places'][b] = child['places'][b], child['places'][a]
+
+            for ii in range(0, len(child['places'])-1):
+                for jj in range(ii+1, len(child['places'])):
+                    if (jj - ii) == abs(child['places'][jj] - child['places'][ii]):
+                        child['errors'] += 1
+            if min_error[1] > child['errors']:
+                min_error = (child['places'], child['errors'])
+            if min_error[1] == 0:
+                break
+
             children.append(child)
+            if len(children) >= limit or min_error[1] == 0:
+                break
 
-        if len(children) >= limit:
+        if len(children) >= limit or min_error[1] == 0:
             break
 
-    min_error = (0, sys.maxint)
-    for child in children:
-        for i in range(0, len(child['places'])-1):
-            for j in range(i+1, len(child['places'])):
-                if (j - i) == abs(child['places'][j] - child['places'][i]):
-                    child['errors'] += 1
-        if min_error[1] > child['errors']:
-            min_error = (child['places'], child['errors'])
-        if min_error[1] == 0:
-            break
-
+    print(len(children))
     return min_error[1], min_error[0], children
 
 if __name__ == '__main__':
     args = parse_args()
-    N = args.size
+    N = args.board_size
+    pop_limit = args.limit
+    mutation = args.mutation if args.mutation else 0
 
     retries = 0
-    min_error = sys.maxint
+    error = sys.maxint
 
-    while min_error > 0:
+    while error > 0:
         parents = [{'places': [x for x in range(0, N)], 'errors': 0} for x in range(0, N)]
         for parent in parents:
             random.shuffle(parent['places'])
 
-        min_error = sys.maxint
+        error = sys.maxint
         best_child = []
-        while min_error > 0:
-            if len(parents) <= 0 or len(parents) >= args.limit:
-                break
-            min_error, best_child, parents = get_children(parents, args.limit)
 
-        print(min_error, best_child)
+        start = time.time()
+        while error > 0:
+            if len(parents) <= 0 or len(parents) >= pop_limit:
+                break
+
+            error, best_child, parents = get_children(parents, limit=pop_limit, board_size=N, mutation=mutation)
+        end = time.time()
+
+        print("Epoch = {}s".format(end - start))
+        print(error, best_child)
